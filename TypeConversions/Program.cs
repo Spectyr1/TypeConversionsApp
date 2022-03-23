cedd84785b56032e89b9d6303d99914b912472c1
Danny Warren <danny@theforbiddenarts.com>
2022-03-23 00:29:11 -0400

    code commit

--- TypeConversions/Program.cs
+++ TypeConversions/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.Write("What is your age: ");
+string? ageText = Console.ReadLine();
+
+//Console.WriteLine(ageText + 15);
+
+//int age = int.Parse(ageText);
+
+//int age;
+
+bool isValidInt = int.TryParse(ageText, out int age);
+
+Console.WriteLine($"This is valid: {isValidInt}. The number is {age}.");
+
+Console.WriteLine(age + 15);
+
+double testDouble = age;
+decimal testDecimal = (decimal)testDouble;
+
